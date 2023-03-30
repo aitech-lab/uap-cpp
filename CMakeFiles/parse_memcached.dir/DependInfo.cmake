eb6906dfc4569daf5d3bc61429ef5c2dbc4712f2
v.seregin <v.seregin@aitarget.com>
2023-03-30 18:22:02 +0300

    memcached

---
+++ CMakeFiles/parse_memcached.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/peko/prj/aitarget/uap-cpp/MurmurHash2.cpp" "CMakeFiles/parse_memcached.dir/MurmurHash2.cpp.o" "gcc" "CMakeFiles/parse_memcached.dir/MurmurHash2.cpp.o.d"
+  "/home/peko/prj/aitarget/uap-cpp/base64.cpp" "CMakeFiles/parse_memcached.dir/base64.cpp.o" "gcc" "CMakeFiles/parse_memcached.dir/base64.cpp.o.d"
+  "/home/peko/prj/aitarget/uap-cpp/parse_memcached.cpp" "CMakeFiles/parse_memcached.dir/parse_memcached.cpp.o" "gcc" "CMakeFiles/parse_memcached.dir/parse_memcached.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/peko/prj/aitarget/uap-cpp/CMakeFiles/uap-cpp-static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/parse_memcached.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for parse_memcached.
+# This may be replaced when dependencies are built.
